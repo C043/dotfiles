02b0742bf5683cac71f7725d90a6ff897fc63fd5
C043 <mariofragnitoph@gmail.com>
2025-06-03 20:40:26 +0200

    Changed remaps for buffers traversing

--- nvim/.config/nvim/lua/remaps.lua
+++ nvim/.config/nvim/lua/remaps.lua
@@ -72,8 +72,6 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
-vim.keymap.set("n", "<leader>0", "<cmd>Dashboard<CR>", { desc = "Open DashBoard" })
-
 -- Debugger keymaps
 vim.keymap.set("n", "<F5>", function()
 	require("dap").continue()
@@ -131,15 +129,16 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set("n", "<leader>1", "<Cmd>BufferGoto 1<CR>")
-vim.keymap.set("n", "<leader>2", "<Cmd>BufferGoto 2<CR>")
-vim.keymap.set("n", "<leader>3", "<Cmd>BufferGoto 3<CR>")
-vim.keymap.set("n", "<leader>4", "<Cmd>BufferGoto 4<CR>")
-vim.keymap.set("n", "<leader>5", "<Cmd>BufferGoto 5<CR>")
-vim.keymap.set("n", "<leader>6", "<Cmd>BufferGoto 6<CR>")
-vim.keymap.set("n", "<leader>7", "<Cmd>BufferGoto 7<CR>")
-vim.keymap.set("n", "<leader>8", "<Cmd>BufferGoto 8<CR>")
-vim.keymap.set("n", "<leader>9", "<Cmd>BufferGoto 9<CR>")
+vim.keymap.set("n", "<C-1>", "<Cmd>BufferGoto 1<CR>")
+vim.keymap.set("n", "<C-2>", "<Cmd>BufferGoto 2<CR>")
+vim.keymap.set("n", "<C-3>", "<Cmd>BufferGoto 3<CR>")
+vim.keymap.set("n", "<C-4>", "<Cmd>BufferGoto 4<CR>")
+vim.keymap.set("n", "<C-5>", "<Cmd>BufferGoto 5<CR>")
+vim.keymap.set("n", "<C-6>", "<Cmd>BufferGoto 6<CR>")
+vim.keymap.set("n", "<C-7>", "<Cmd>BufferGoto 7<CR>")
+vim.keymap.set("n", "<C-8>", "<Cmd>BufferGoto 8<CR>")
+vim.keymap.set("n", "<C-9>", "<Cmd>BufferGoto 9<CR>")
+vim.keymap.set("n", "<C-0>", "<cmd>Dashboard<CR>", { desc = "Open DashBoard" })
 
 -- Visual Mode
 -- Visual mode mappings to move lines up and down
