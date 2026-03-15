d74f6bd2ec9f84981a5f9c127d6ebb1c56f3c34a
C043 <mariofragnitoph@gmail.com>
2026-03-15 17:17:23 +0100

    Adds nvimLint.lua

---
+++ nvim/.config/nvim/lua/plugins/nvimLint.lua
@@ -0,0 +1,14 @@
+return {
+	"mfussenegger/nvim-lint",
+	config = function()
+		require("lint").linters_by_ft = {
+			json = { "jsonlint" },
+		}
+
+		vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
+	end,
+}
