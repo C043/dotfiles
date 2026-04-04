6fdecb841ac41b33b63dfa32768a15916148ead4
C043 <mariofragnitoph@gmail.com>
2026-04-04 22:16:58 +0200

    Migrated treesitter with new version

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,45 +1,58 @@
+local highlight_filetypes = {
+	"bash",
+	"c",
+	"diff",
+	"html",
+	"javascript",
+	"lua",
+	"luadoc",
+	"markdown",
+	"query",
+	"typescript",
+	"vim",
+}
+
+local indent_filetypes = {
+	"bash",
+	"c",
+	"diff",
+	"jsdoc",
+	"lua",
+	"luadoc",
+	"markdown",
+	"query",
+	"typescript",
+	"vim",
+}
+
 return {
-	{ -- Highlight, edit, and navigate code
+	{
 		"nvim-treesitter/nvim-treesitter",
 		branch = "main",
+		lazy = false,
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
-		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-				"javascript",
-				"jsdoc",
-				"typescript",
-			},
+		config = function()
+			local treesitter = require("nvim-treesitter")
 
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby", "html", "javascript", "ejs" },
-			},
-			indent = { enable = true, disable = { "ruby", "html", "javascript" } },
-		},
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
+			treesitter.setup()
 
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+			local group = vim.api.nvim_create_augroup("dotfiles-treesitter", { clear = true })
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				pattern = highlight_filetypes,
+				callback = function(args)
+					pcall(vim.treesitter.start, args.buf)
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				pattern = indent_filetypes,
+				callback = function(args)
+					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
 	},
 }
