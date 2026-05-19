dfdb6a9ad8a0f68c9265ffb880f11f9154111db2
C043 <mariofragnitoph@gmail.com>
2026-05-19 11:40:04 +0200

    fix(audio): replace broken razer_soft with direct default

--- nixos/etc/nixos/modules/audio.nix
+++ nixos/etc/nixos/modules/audio.nix
@@ -10,22 +10,15 @@
     alsa.enable = true;
     alsa.support32Bit = true;
 
-    extraConfig.pipewire-pulse."51-razer-softvol" = {
-      "pulse.cmd" = [
+    wireplumber.extraConfig."51-default-sink" = {
+      "monitor.alsa.rules" = [
         {
-          cmd = "load-module";
-          args = "module-null-sink sink_name=razer_soft sink_properties=device.description=Razer_Software_Volume";
-          flags = [ "nofail" ];
-        }
-        {
-          cmd = "load-module";
-          args = "module-loopback source=razer_soft.monitor sink=alsa_output.usb-Razer_Razer_Leviathan_V2_X_000000000000000-01.analog-stereo latency_msec=20";
-          flags = [ "nofail" ];
-        }
-        {
-          cmd = "set-default-sink";
-          args = "razer_soft";
-          flags = [ "nofail" ];
+          matches = [
+            { "node.name" = "alsa_output.usb-Razer_Razer_Leviathan_V2_X_000000000000000-01.iec958-stereo"; }
+          ];
+          actions.update-props = {
+            "priority.session" = 10000;
+          };
         }
       ];
     };
