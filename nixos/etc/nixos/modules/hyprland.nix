247920281859c08da3543da190cead7da80b6bd5
C043 <mariofragnitoph@gmail.com>
2026-05-21 09:47:10 +0200

    fix(hyprland): route xdg portals to correct backends

--- nixos/etc/nixos/modules/hyprland.nix
+++ nixos/etc/nixos/modules/hyprland.nix
@@ -3,6 +3,23 @@
 {
   programs.hyprland.enable = true;
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
+    config = {
+      hyprland = {
+        default = [ "hyprland" "gtk" ];
+        "org.freedesktop.impl.portal.ScreenCast" = "hyprland";
+        "org.freedesktop.impl.portal.Screenshot" = "hyprland";
+        "org.freedesktop.impl.portal.GlobalShortcuts" = "hyprland";
+        "org.freedesktop.impl.portal.FileChooser" = "gtk";
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     hypridle
     hyprlock
