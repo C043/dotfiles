47b039e95544f725575e1855a8d2398b2b0a26c6
C043 <mariofragnitoph@gmail.com>
2026-05-22 18:02:36 +0200

    Enhanced movefocus-warp.sh

--- hyprland/.config/hypr/scripts/movefocus-warp.sh
+++ hyprland/.config/hypr/scripts/movefocus-warp.sh
@@ -6,10 +6,16 @@ direction="$1"
 hyprctl dispatch movefocus "$direction"
 
 win=$(hyprctl activewindow -j)
-[ -z "$win" ] && exit 0
+addr=$(echo "$win" | jq -r '.address // empty')
 
-x=$(echo "$win" | jq '.at[0] // empty')
-y=$(echo "$win" | jq '.at[1] // empty')
-[ -z "$x" ] || [ -z "$y" ] && exit 0
-
-hyprctl dispatch movecursor $((x + 10)) $((y + 10))
+if [ -n "$addr" ] && [ "$addr" != "0x" ]; then
+    x=$(echo "$win" | jq '.at[0] // empty')
+    y=$(echo "$win" | jq '.at[1] // empty')
+    [ -z "$x" ] || [ -z "$y" ] && exit 0
+    hyprctl dispatch movecursor $((x + 10)) $((y + 10))
+else
+    mon=$(hyprctl activeworkspace -j | jq -r '.monitor // empty')
+    [ -z "$mon" ] && exit 0
+    eval "$(hyprctl monitors -j | jq -r --arg m "$mon" '.[] | select(.name==$m) | "mx=\(.x) my=\(.y) mw=\(.width) mh=\(.height)"')"
+    hyprctl dispatch movecursor $((mx + mw / 2)) $((my + mh / 2))
+fi
